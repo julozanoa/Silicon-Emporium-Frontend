5814a0150ec6b37cd54708384b185609407129d7
Alerodriguezmar <alerodriguezmar@unal.edu.co>
2022-11-23 19:47:49 -0500

    visual fix login and implement reactive forms

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -1,7 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Router } from '@angular/router';
 import { Login } from '../Model';
 import { RegisterService } from '../Services/register.service';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-login-component',
@@ -10,26 +13,42 @@ import { RegisterService } from '../Services/register.service';
 })
 export class LoginComponentComponent implements OnInit {
 
-  logindata:Login = {
-    username: '',
-    password: ''
-  }
+  user!:FormGroup;
+  login!:Login
+  constructor(private router: Router,private registerService: RegisterService,private fb: FormBuilder) { }
 
-  constructor(private router: Router,private registerService: RegisterService) { }
+
 
   ngOnInit(): void {
-  }
-
-
-  login(){
+    this.user = this.initForm();
   
-
-    localStorage.setItem('username',"aaa")
-    this.router.navigate([''])
-    .then(() => {
-      window.location.reload();
-    });
-    
   }
 
+
+
+
+
+
+  onSubmit():void{
+    this.login = this.user.value
+    this.registerService.login(this.login).subscribe(  res =>{
+      localStorage.setItem('username',this.login.username)
+      this.router.navigate(['']).then(() => {
+        window.location.reload();})
+      
+  },
+  err => {
+      window.alert(["Usuario o Contraseña incorrecta"])
+  })
+  }
+
+  initForm():FormGroup{
+   return this.fb.group({
+      username:['',[Validators.required,Validators.minLength(5)]],
+      password:['',[Validators.required,Validators.minLength(5)]]
+    });
+  }
+
+
+  
 }
